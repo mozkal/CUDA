96443a1e1f7584388a2c110d8df6d30bc21c8ac7
mozkal <mm347184@students.mimuw.edu.pl>
2016-10-11 22:56:58 +0200

    Update declarations.h

--- Wunsch-Needleman/declarations.h
+++ Wunsch-Needleman/declarations.h
@@ -23,7 +23,7 @@ std::vector<Fragment> prepare_set();
 char* prepare_seq(const std::vector<Fragment> &seq);
 /* Zakres sąsiedztwa dla granicznych sekwencji jest nieregularny, dlatego część
  * obliczeń pierwszych 128 sekwencji jest doliczana na cpu (a dokładniej
- * 128 + 127 + ... + 1 wywołań needlman_wunsch */
+ * 128 + 127 + ... + 1 wywołań needleman_wunsch */
 int* calculate_cpu(const std::vector<Fragment> &seq);
 int needleman_wunsch(const char* seq1, const char* seq2);
 void print_results(int* cpu_out, int* gpu_out, std::vector<Fragment> const &seq);
